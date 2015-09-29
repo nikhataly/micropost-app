fe7cd57ef07ba1e7af20f4c0348a83906899bc1c
Nikhat Bhayani <nikhataly@gmail.com>
2015-09-29 00:11:03 -0400

    Adding previously unsaved changes in implementing remember user

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,7 +15,7 @@ class ActiveSupport::TestCase
     remember_me = options[:remember_me] || '1'
     if integration_test?
       post login_path, session: { email:       user.email,
-                                  password:    password,
+                                  password:     password,
                                   remember_me: remember_me }
     else
       session[:user_id] = user.id
