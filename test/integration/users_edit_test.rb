b5ad4f4331601e3c42a10d52440d17f7ca67bbbe
Nikhat Bhayani <nikhataly@gmail.com>
2015-09-30 17:02:18 -0400

    Fixing syntax in previous test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,7 +9,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test "unsuccessfull edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path(@user), user : {name: "",
+    patch user_path(@user), user: {name: "",
                                     email:  "foo@invalid",
                                     password: "foo",
                                     password_confirmation: "bar"}
