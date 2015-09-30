9ecf057816041853f77f67a999828cf2dec10edc
Nikhat Bhayani <nikhataly@gmail.com>
2015-09-30 16:58:06 -0400

    Adding setup method in user edit test

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UsersEditTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:nikhat)
+  end
+
 end
