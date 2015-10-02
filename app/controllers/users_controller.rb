cfd1c2c32231f52984d18d3d7b68c86b06247ca8
Nikhat Bhayani <nikhataly@gmail.com>
2015-10-01 22:03:56 -0400

    Adding before filter and method for logged in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -30,6 +32,9 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def logged_in_user
   end
 
   private
