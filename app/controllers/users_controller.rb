9670ffcda6a04cf581c74d69e430a62720db3012
Nikhat Bhayani <nikhataly@gmail.com>
2015-11-27 14:37:15 -0500

    Adding private method to confirm admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,5 +63,9 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
 
