39b2c88df4148fd510b1ef748834134ccef10c8c
Nikhat Bhayani <nikhataly@gmail.com>
2015-11-30 11:27:19 -0500

    Micropost model and test for validity

--- db/migrate/20151130030504_create_microposts.rb
+++ db/migrate/20151130030504_create_microposts.rb
@@ -1,11 +1,10 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateMicroposts < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+    create_table :microposts do |t|
       t.text :content
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
-    add_index :microposts, [:user_id, :created_at]
   end
 end
