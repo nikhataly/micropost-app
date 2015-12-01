e8f575981e6d7ce58aaca6560d1ea505a4821b00
Nikhat Bhayani <nikhataly@gmail.com>
2015-12-01 16:44:10 -0500

    Fixing syntax errors

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build if logged_in?
-      @feed_items = current_user.feed.paginage(page: params[:page])
+      @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -60,6 +60,7 @@ class User < ActiveRecord::Base
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
   private
 
   def downcase_email
