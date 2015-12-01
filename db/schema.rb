a86438dd208a479933dca9d6ccb1f1ddd17252f2
Nikhat Bhayani <nikhataly@gmail.com>
2015-12-01 13:24:49 -0500

    Trying to fix db mess

--- db/schema.rb
+++ db/schema.rb
@@ -11,32 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130030504) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",             default: false
-    t.string   "activation_digest"
-    t.boolean  "activated",         default: false
-    t.datetime "activated_at"
-    t.string   "reset_digest"
-    t.datetime "reset_sent_at"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+ActiveRecord::Schema.define(version: 0) do
 
 end
